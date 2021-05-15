fd8ff0553fae46d8ac0b13d396b8c7957289c169
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-05-15 23:42:47 +0430

    updated Stack.h

---
+++ Stack.h
@@ -0,0 +1,33 @@
+//
+// Created by Leila on 5/3/2021.
+//
+
+#ifndef DATASTRUCTURES_STACK_H
+#define DATASTRUCTURES_STACK_H
+#include <string>
+#include "Array1.h"
+
+
+using namespace std;
+
+   class Stack: public Array1
+    {
+      //  int top;
+      //  int size;
+
+        bool full_st();
+        bool empty_st();
+
+    public:
+        Stack(int s1);
+
+        bool push_st(int num);
+
+        double pop_st();
+
+       ~Stack();
+    };
+
+
+
+#endif //DATASTRUCTURES_STACK_H
